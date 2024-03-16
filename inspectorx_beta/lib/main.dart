9197c77f0e212c4e5e4414996500cf426855711f
Reuben Reny <reubenreny@karunya.edu.in>
2024-03-17 03:39:48 +0530

    Splash Screen Added

--- inspectorx_beta/lib/main.dart
+++ inspectorx_beta/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:inspectorx_beta/firebase_options.dart';
 import 'package:inspectorx_beta/pages/auth_page.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:inspectorx_beta/splash_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,7 +21,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: AuthPage(),
+      // home: AuthPage(),
+      home: SplashScreen(),
     );
   }
 }
---
+++ inspectorx_beta/lib/splash_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:inspectorx_beta/pages/auth_page.dart';
+import 'package:inspectorx_beta/pages/home_page.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Future.delayed(const Duration(seconds: 2), () {
+      Navigator.of(context)
+          .pushReplacement(MaterialPageRoute(builder: (_) => const AuthPage()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+            decoration: const BoxDecoration(
+              color: Color(0xff162535),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Center(
+                  child: Image.asset("lib/images/banner.png", height: 100),
+                )
+              ],
+            ))
+        // ignore: dead_code
+        );
+  }
+}
